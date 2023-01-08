7b2d8c3a417a7e4699cb443daa0038e50d231187
Jan Macenka <jan@macenka.de>
2023-01-08 23:27:54 +0100

    Updated installation of basic dependencies to fixe broken ffi-package-installation

--- install_basic_dependencies.sh
+++ install_basic_dependencies.sh
@@ -3,7 +3,7 @@
 
 echo "NOW: Install basic dependencies..."
 sudo apt update
-sudo apt install ruby-full build-essential zlib1g-dev git docker docker-compose
+sudo apt install clang make ruby-dev libffi-dev ruby-full build-essential zlib1g-dev git docker docker-compose
 
 echo '# Install Ruby Gems to ~/gems' >> ~/.bashrc
 echo 'export GEM_HOME="$HOME/gems"' >> ~/.bashrc
